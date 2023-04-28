a975bc562fcb0db2e9a82df13715fb2ca00a5e01
Trimero <rusaxius@gmail.com>
2023-04-28 11:17:22 +0300

    feat: added wrappers and styles (Fri, Apr 28, 2023 08:17:22 AM)

---
+++ script.js
@@ -0,0 +1,11 @@
+const mainWrapper = document.createElement('div');
+mainWrapper.classList.add('main-wrapper');
+document.body.append(mainWrapper);
+
+const inputTextarea = document.createElement('textarea');
+inputTextarea.classList.add('input-field');
+mainWrapper.append(inputTextarea);
+
+const keyboardContainer = document.createElement('div');
+keyboardContainer.classList.add('keyboard-container');
+mainWrapper.append(keyboardContainer);
